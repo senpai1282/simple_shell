aa19c221a78b6eb0b085969758888ef2a510ed6e
senpai1282 <farhi.ayoub.hamza@gmail.com>
2023-12-26 12:27:58 -0800

    task 1

--- 0.get_pid.c
+++ 0.get_pid.c
@@ -1,20 +1,22 @@
 #include <unistd.h>
 #include <stdio.h>
 /**
- * getpid - function returns the process ID of the current process.
- * getppid - function returns the process ID of the parent of the current process.
+ * getpid - returns the process ID of the current process.
+ * getppid - returns the process ID of the parent of the current process.
+ * main = pid/ppid
+ *
  * list_pid = lists the PID of the prossess.
  * list_ppid = lists the PPID of the prosess.
- * return = (0)
+ * return : 0
  */
-
 int main(void)
 {
 	pid_t list_pid;
 		list_pid = getpid();
-	       	printf("The PID is: %u\n", list_pid);
+		printf("The PID is: %u\n", list_pid);
 	pid_t list_ppid;
 		list_ppid = getppid();
-		printf("The PID is: %u\n", list_ppid);	
-    return (0);
+		printf("The PPID is:%u\n", list_ppid);
+	return (0);
 }
+
