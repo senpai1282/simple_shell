7ff2453461b3c28e58618900cc2380249b35d6df
senpai1282 <farhi.ayoub.hamza@gmail.com>
2024-01-01 17:30:33 -0800

    fist task

--- 0.get_pid.c
+++ 0.get_pid.c
@@ -1,26 +1,28 @@
 #include <unistd.h>
 #include <stdio.h>
+
 /**
- * getpid - returns the process ID of the current process.
- * getppid - returns the process ID of the parent of the current process.
- * main = pid/ppid
+ * main - Entry point of the program.
+ * Description: This function retrieves and prints the Process ID (PID)
+ *              and Parent Process ID (PPID) of the current process.
  *
  * list_pid = lists the PID of the prossess.
  * list_ppid = lists the PPID of the prosess.
- * return : 0
+ * Return: 0
  */
 
 int main(void)
-
 {
-	pid_t list_pid; {
-		list_pid = getpid();
-		printf("The PID is: %u\n", list_pid);
-	}
-	pid_t list_ppid; {
-		list_ppid = getppid();
-		printf("The PPID is:%u\n", list_ppid);
-	}
+	pid_t list_pid;
+
+	list_pid = getpid();
+	printf("The PID is: %u\n", list_pid);
+
+	pid_t list_ppid;
+
+	list_ppid = getppid();
+	printf("The PPID is: %u\n", list_ppid);
+
 	return (0);
 }
 
