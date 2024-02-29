201db5603406b8c3b1e1d792861c64958f1007b7
senpai1282 <farhi.ayoub.hamza@gmail.com>
2024-02-28 17:40:17 -0800

    hi

---
+++ main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+// Function declarations
+void ls(void);
+void execute_command(char *command);
+
+#endif /* MAIN_H */
