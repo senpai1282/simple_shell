d58ef585460821c18ad0fc0101eca95e308f5751
Ayoub farhi <57156582+senpai1282@users.noreply.github.com>
2024-02-29 03:00:41 +0000

    hi

---
+++ string_funcs.c
@@ -0,0 +1,53 @@
+#include "header.h"
+
+/**
+ * _strlen - Calculate the length of a string.
+ * @s: The input string.
+ * Return: The length of the string.
+ */
+size_t _strlen(const char *s)
+{
+    size_t len = 0;
+
+    while (s[len] != '\0')
+        len++;
+
+    return len;
+}
+
+/**
+ * _strcpy - Copy a string.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ * Return: A pointer to the destination buffer.
+ */
+char *_strcpy(char *dest, const char *src)
+{
+    size_t i;
+
+    for (i = 0; src[i] != '\0'; i++)
+        dest[i] = src[i];
+
+    dest[i] = '\0';
+
+    return dest;
+}
+
+/**
+ * _strcat - Concatenate two strings.
+ * @dest: The destination buffer.
+ * @src: The source string.
+ * Return: A pointer to the destination buffer.
+ */
+char *_strcat(char *dest, const char *src)
+{
+    size_t dest_len = _strlen(dest);
+    size_t i;
+
+    for (i = 0; src[i] != '\0'; i++)
+        dest[dest_len + i] = src[i];
+
+    dest[dest_len + i] = '\0';
+
+    return dest;
+}
