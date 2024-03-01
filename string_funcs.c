787e64cdb7f2f78d64ed055be24737d9621e9d1c
Ayoub farhi <57156582+senpai1282@users.noreply.github.com>
2024-03-01 15:38:08 -0500

    Update string_funcs.c

--- string_funcs.c
+++ string_funcs.c
@@ -7,12 +7,12 @@
  */
 size_t _strlen(const char *s)
 {
-    size_t len = 0;
+	size_t len = 0;
+	
+	while (s[len] != '\0')
+		len++;
 
-    while (s[len] != '\0')
-        len++;
-
-    return len;
+	return len;
 }
 
 /**
@@ -23,14 +23,14 @@ size_t _strlen(const char *s)
  */
 char *_strcpy(char *dest, const char *src)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; src[i] != '\0'; i++)
-        dest[i] = src[i];
+	for (i = 0; src[i] != '\0'; i++)
+		dest[i] = src[i];
 
-    dest[i] = '\0';
+	dest[i] = '\0';
 
-    return dest;
+	return dest;
 }
 
 /**
@@ -41,13 +41,13 @@ char *_strcpy(char *dest, const char *src)
  */
 char *_strcat(char *dest, const char *src)
 {
-    size_t dest_len = _strlen(dest);
-    size_t i;
+	size_t dest_len = _strlen(dest);
+	size_t i;
 
-    for (i = 0; src[i] != '\0'; i++)
-        dest[dest_len + i] = src[i];
+	for (i = 0; src[i] != '\0'; i++)
+		dest[dest_len + i] = src[i];
 
-    dest[dest_len + i] = '\0';
+	dest[dest_len + i] = '\0';
 
-    return dest;
+	return dest;
 }
