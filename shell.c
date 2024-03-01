6d748f408c5658270f494ef611fdb3d255a81da9
senpai1282 <farhi.ayoub.hamza@gmail.com>
2024-03-01 09:36:56 -0800

    hi

--- shell.c
+++ shell.c
@@ -8,73 +8,73 @@
 
 void ls(void) 
 {
-    struct dirent *entry;
-    DIR *dir = opendir("."); 
+	struct dirent *entry;
+	DIR *dir = opendir("."); 
     
-    if (dir == NULL) 
-    {
-        perror("opendir");
-        exit(EXIT_FAILURE);
-    }
-    
-    while ((entry = readdir(dir)) != NULL) 
-    {
-        write(STDOUT_FILENO, entry->d_name, strlen(entry->d_name));
-        write(STDOUT_FILENO, "\n", 1); 
-    }
+	if (dir == NULL) 
+	{
+	perror("opendir");
+	exit(EXIT_FAILURE);
+	}
+	while ((entry = readdir(dir)) != NULL)
+	{
+	write(STDOUT_FILENO, entry->d_name, strlen(entry->d_name));
+	write(STDOUT_FILENO, "\n", 1); 
+	}
 
-    closedir(dir);
+	closedir(dir);
 }
 
 void echo(int argc, char *argv[]) 
 {
-    int i;
-    for (i = 1; i < argc; i++) 
-    {
+	int i;
+	for (i = 1; i < argc; i++) 
+	{
         write(STDOUT_FILENO, argv[i], strlen(argv[i]));
         if (i < argc - 1) 
-        {
-            write(STDOUT_FILENO, " ", 1);
-        }
-    }
-    write(STDOUT_FILENO, "\n", 1);
+		{
+		write(STDOUT_FILENO, " ", 1);
+		}
+	}
+	write(STDOUT_FILENO, "\n", 1);
 }
 
-int main() 
+int main()
 {
-    char *line = NULL;
-    size_t len = 0;
-    ssize_t read;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
 
-    while (1) 
-    {
-        write(STDOUT_FILENO, "#cisfun$  ", 16);
-        read = getline(&line, &len, stdin);
-        if (read != -1) 
-        {
-            line[strcspn(line, "\n")] = '\0';
+	while (1) 
+	{
+	write(STDOUT_FILENO, "#cisfun$  ", 16);
+	read = getline(&line, &len, stdin);
+		if (read != -1) 
+		{
+		line[strcspn(line, "\n")] = '\0';
 
-            if (strcmp(line, "ls") == 0) 
-            {
-                ls();
-            } else if (strcmp(line, "echo") == 0) 
-            {
-                char *token = strtok(line, " ");
-                int argc = 0;
-                char *argv[10];
+		if (strcmp(line, "ls") == 0) 
+		{
+		ls();
+		}
+		else if (strcmp(line, "echo") == 0) 
+	{
+	char *token = strtok(line, " ");
+	int argc = 0;
+	char *argv[10];
 
-                while (token != NULL && argc < 10) 
-                {
-                    argv[argc++] = token;
-                    token = strtok(NULL, " ");
-                }
+		while (token != NULL && argc < 10) 
+		{
+			argv[argc++] = token;
+			token = strtok(NULL, " ");
+			}
 
-                echo(argc, argv);
-            }
+                	echo(argc, argv);
+			}
             
-        }
-    }
+		}
+	}
 
-    free(line);
-    return 0;
+	free(line);
+	return 0;
 }
