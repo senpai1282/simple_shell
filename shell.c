56d3b6c5a67f9dcfbedbe858fbc68f5b5dc8e96a
Ayoub farhi <57156582+senpai1282@users.noreply.github.com>
2024-03-01 11:13:37 -0500

    Update shell.c

--- shell.c
+++ shell.c
@@ -6,17 +6,19 @@
 #include <dirent.h>
 #include <string.h>
 
-ls(void) 
+void ls(void) 
 {
     struct dirent *entry;
     DIR *dir = opendir("."); 
     
-    if (dir == NULL) {
+    if (dir == NULL) 
+    {
         perror("opendir");
         exit(EXIT_FAILURE);
     }
     
-    while ((entry = readdir(dir)) != NULL) {
+    while ((entry = readdir(dir)) != NULL) 
+    {
         write(STDOUT_FILENO, entry->d_name, strlen(entry->d_name));
         write(STDOUT_FILENO, "\n", 1); 
     }
@@ -24,36 +26,45 @@ ls(void)
     closedir(dir);
 }
 
-void echo(int argc, char *argv[]) {
+void echo(int argc, char *argv[]) 
+{
     int i;
-    for (i = 1; i < argc; i++) {
+    for (i = 1; i < argc; i++) 
+    {
         write(STDOUT_FILENO, argv[i], strlen(argv[i]));
-        if (i < argc - 1) {
+        if (i < argc - 1) 
+        {
             write(STDOUT_FILENO, " ", 1);
         }
     }
     write(STDOUT_FILENO, "\n", 1);
 }
 
-int main() {
+int main() 
+{
     char *line = NULL;
     size_t len = 0;
     ssize_t read;
 
-    while (1) {
+    while (1) 
+    {
         write(STDOUT_FILENO, "#cisfun$  ", 16);
         read = getline(&line, &len, stdin);
-        if (read != -1) {
+        if (read != -1) 
+        {
             line[strcspn(line, "\n")] = '\0';
 
-            if (strcmp(line, "ls") == 0) {
+            if (strcmp(line, "ls") == 0) 
+            {
                 ls();
-            } else if (strcmp(line, "echo") == 0) {
+            } else if (strcmp(line, "echo") == 0) 
+            {
                 char *token = strtok(line, " ");
                 int argc = 0;
                 char *argv[10];
 
-                while (token != NULL && argc < 10) {
+                while (token != NULL && argc < 10) 
+                {
                     argv[argc++] = token;
                     token = strtok(NULL, " ");
                 }
